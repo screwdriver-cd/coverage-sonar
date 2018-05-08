54368d8e1495d6382bed0979e20025458b3f1eb7
Min Zhang <minzhangcmu@gmail.com>
2018-05-07 17:36:44 -0700

    feat: implement sonar plugin (#1). BREAKING CHANGE: bump major

---
+++ index.js
@@ -0,0 +1,167 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+
+'use strict';
+
+const fs = require('fs');
+const joi = require('joi');
+const path = require('path');
+const request = require('request-promise-native');
+const uuidv4 = require('uuid/v4');
+const CoverageBase = require('screwdriver-coverage-base');
+
+const COMMANDS = fs.readFileSync(path.join(__dirname, 'commands.txt'), 'utf8').trim();
+
+let adminToken;
+let sonarHost;
+let sdApiAuthUrl;
+
+/**
+ * Create a project in sonar
+ * @method createProject
+ * @param  {String} projectKey    Unique identifier for the project
+ * @return {Promise}              Project object if it gets created or empty object if already exists
+ */
+function createProject(projectKey) {
+    return request({
+        json: true,
+        method: 'POST',
+        // eslint-disable-next-line max-len
+        uri: `${sonarHost}/api/projects/create?project=${projectKey}&name=${projectKey}&visibility=private`,
+        auth: {
+            username: adminToken
+        }
+    }).catch((err) => {
+        if (err.statusCode === 400 && err.message.includes('already exists')) {
+            return {};
+        }
+
+        throw new Error(`Failed to create project ${projectKey}: ${err.message}`);
+    });
+}
+
+/**
+ * Create a user in sonar
+ * @method createUser
+ * @param  {String} username  Username of the user
+ * @param  {String} password  Password of the user
+ * @return {Promise}          User object if it gets created or empty object if already exists
+ */
+function createUser(username, password) {
+    return request({
+        json: true,
+        method: 'POST',
+        // eslint-disable-next-line max-len
+        uri: `${sonarHost}/api/users/create?login=${username}&name=${username}&password=${password}`,
+        auth: {
+            username: adminToken
+        }
+    }).catch((err) => {
+        if (err.statusCode === 400 && err.message.includes('already exists')) {
+            return {};
+        }
+
+        throw new Error(`Failed to create user ${username}: ${err.message}`);
+    });
+}
+
+/**
+ * Give specific user push access to the project
+ * @method grantUserPermission
+ * @param  {String} username      Username of the user
+ * @param  {String} projectKey    Unique identifier for the project
+ * @return {Promise}              Nothing if permission granted
+ */
+function grantUserPermission(username, projectKey) {
+    // Always return 204 even with duplicate calls
+    return request({
+        json: true,
+        method: 'POST',
+        // eslint-disable-next-line
+        uri: `${sonarHost}/api/permissions/add_user?login=${username}&permission=scan&projectKey=${projectKey}`,
+        auth: {
+            username: adminToken
+        }
+    }).catch((err) => {
+        throw new Error(`Failed to grant user ${username} permission: ${err.message}`);
+    });
+}
+
+/**
+ * Generate an access token for the given user
+ * @method generateToken
+ * @param  {String} username   username of the user
+ * @return {Promise}           Object with a token field
+ */
+function generateToken(username) {
+    const tokenName = uuidv4();
+
+    return request({
+        json: true,
+        method: 'POST',
+        uri: `${sonarHost}/api/user_tokens/generate?login=${username}&name=${tokenName}`,
+        auth: {
+            username: adminToken
+        }
+    }).catch((err) => {
+        throw new Error(`Failed to generate user ${username} token: ${err.message}`);
+    });
+}
+
+class CoverageSonar extends CoverageBase {
+    /**
+     * Constructor
+     * @method constructor
+     * @param  {Object} config              Configuration object
+     * @param  {String} config.sdApiUrl     URL for Screwdriver API
+     * @param  {String} config.sonarHost    Host for SonarQube Server
+     * @param  {String} config.adminToken   Sonar Admin token
+     */
+    constructor(config) {
+        super();
+
+        this.config = joi.attempt(config, joi.object().keys({
+            sdApiUrl: joi.string().uri().required(),
+            sonarHost: joi.string().uri().required(),
+            adminToken: joi.string().required()
+        }).unknown(true), 'Invalid config for sonar coverage plugin');
+
+        sdApiAuthUrl = `${this.config.sdApiUrl}/v4/coverage/token`;
+        adminToken = this.config.adminToken;
+        sonarHost = this.config.sonarHost;
+        this.uploadCommands = COMMANDS
+            .replace('$SD_API_AUTH_URL', sdApiAuthUrl)
+            .replace('$SONAR_HOST', sonarHost)
+            .split('\n');
+        this.uploadCommands[this.uploadCommands.length - 1] += ' || true';
+    }
+
+    /**
+     * Return an access token that build can use to talk to coverage server
+     * @method getAccessToken
+     * @param {Object} buildCredentials    Infomation stored in a build JWT
+     * @return {Promise}                   An access token that build can use to talk to coverage server
+     */
+    _getAccessToken(buildCredentials) {
+        const { jobId } = buildCredentials;
+        const projectKey = `job:${jobId}`;
+        const username = `user-job-${jobId}`;
+        const password = uuidv4();
+
+        return createProject(projectKey)
+            .then(() => createUser(username, password))
+            .then(() => grantUserPermission(username, projectKey))
+            .then(() => generateToken(username))
+            .then(res => res.token);
+    }
+
+    /**
+     * Get shell command to upload coverage to server
+     * @method _getUploadCoverageCmd
+     * @return {Promise}     Shell commands to upload coverage
+     */
+    _getUploadCoverageCmd() {
+        return Promise.resolve(this.uploadCommands.join(' && '));
+    }
+}
+
+module.exports = CoverageSonar;
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,153 @@
 'use strict';
 
 const assert = require('chai').assert;
+const mockery = require('mockery');
+const sinon = require('sinon');
+const fs = require('fs');
+const path = require('path');
+
+sinon.assert.expose(assert, { prefix: '' });
 
 describe('index test', () => {
-    it('fails', () => {
-        assert.isTrue(false);
+    const config = {
+        sdApiUrl: 'https://api.screwdriver.cd',
+        sonarHost: 'https://sonar.screwdriver.cd',
+        adminToken: 'faketoken'
+    };
+    let SonarPlugin;
+    let sonarPlugin;
+    let requestMock;
+
+    before(() => {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+    });
+
+    beforeEach(() => {
+        requestMock = sinon.stub().resolves(null);
+        mockery.registerMock('request-promise-native', requestMock);
+
+        // eslint-disable-next-line global-require
+        SonarPlugin = require('..');
+
+        sonarPlugin = new SonarPlugin(config);
+    });
+
+    afterEach(() => {
+        mockery.deregisterAll();
+        mockery.resetCache();
+    });
+
+    after(() => {
+        mockery.disable();
+    });
+
+    describe('constructor', () => {
+        it('constructs', () => {
+            assert.ok(sonarPlugin);
+            assert.property(sonarPlugin, 'getAccessToken');
+            assert.property(sonarPlugin, 'getUploadCoverageCmd');
+            assert.deepEqual(sonarPlugin.config, config);
+        });
+    });
+
+    describe('getUploadCoverageCmd', () => {
+        it('constructs upload coverage command correctly', () => {
+            const commandsPath = path.resolve(__dirname, './data/commands.txt');
+            const commands = fs.readFileSync(commandsPath, 'utf8').replace('\n', '');
+
+            return sonarPlugin.getUploadCoverageCmd().then(result =>
+                assert.deepEqual(result, commands)
+            );
+        });
+    });
+
+    describe('getAccessToken', () => {
+        const buildCredentials = { jobId: 1 };
+
+        it('gets an access token successfully', () => {
+            requestMock.onCall(3).resolves({ token: 'accesstoken' });
+
+            return sonarPlugin.getAccessToken(buildCredentials).then((result) => {
+                assert.callCount(requestMock, 4);
+                assert.strictEqual(result, 'accesstoken');
+            });
+        });
+
+        it('gets an access token successfully with existing pipeline', () => {
+            requestMock.onCall(0).rejects({
+                statusCode: 400,
+                message: 'Project already exists.'
+            });
+            requestMock.onCall(3).resolves({ token: 'accesstoken' });
+
+            return sonarPlugin.getAccessToken(buildCredentials).then((result) => {
+                assert.callCount(requestMock, 4);
+                assert.strictEqual(result, 'accesstoken');
+            });
+        });
+
+        it('gets an access token successfully with existing user', () => {
+            requestMock.onCall(1).rejects({
+                statusCode: 400,
+                message: 'user already exists.'
+            });
+            requestMock.onCall(3).resolves({ token: 'accesstoken' });
+
+            return sonarPlugin.getAccessToken(buildCredentials).then((result) => {
+                assert.callCount(requestMock, 4);
+                assert.strictEqual(result, 'accesstoken');
+            });
+        });
+
+        it('throws err if failed to create/locate projects', () => {
+            requestMock.onCall(0).rejects({
+                statusCode: 500,
+                message: '500 - internal server error'
+            });
+
+            return sonarPlugin.getAccessToken(buildCredentials).then(() => {
+                assert.throws(new Error('should not get here'));
+            }).catch(err => assert.deepEqual(err.message,
+                'Failed to create project job:1: 500 - internal server error'));
+        });
+
+        it('throws err if failed to create/locate user', () => {
+            requestMock.onCall(1).rejects({
+                statusCode: 500,
+                message: '500 - internal server error'
+            });
+
+            return sonarPlugin.getAccessToken(buildCredentials).then(() => {
+                assert.throws(new Error('should not get here'));
+            }).catch(err => assert.deepEqual(err.message,
+                'Failed to create user user-job-1: 500 - internal server error'));
+        });
+
+        it('throws err if failed to grant user permission', () => {
+            requestMock.onCall(2).rejects({
+                statusCode: 500,
+                message: '500 - internal server error'
+            });
+
+            return sonarPlugin.getAccessToken(buildCredentials).then(() => {
+                assert.throws(new Error('should not get here'));
+            }).catch(err => assert.deepEqual(err.message,
+                'Failed to grant user user-job-1 permission: 500 - internal server error'));
+        });
+
+        it('it throws err if failed to generate user token', () => {
+            requestMock.onCall(3).rejects({
+                statusCode: 500,
+                message: '500 - internal server error'
+            });
+
+            return sonarPlugin.getAccessToken(buildCredentials).then(() => {
+                assert.throws(new Error('should not get here'));
+            }).catch(err => assert.deepEqual(err.message,
+                'Failed to generate user user-job-1 token: 500 - internal server error'));
+        });
     });
 });
